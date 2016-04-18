cf5d26f573ac32936248d88b75e3ad5ec2abb2e9
tavaresdong <tavares.nju@gmail.com>
2016-04-18 00:20:53 -0700

    16.49 16.50 ok

--- c++_primer_5e/ch16/ex16.48.49.50.cpp
+++ c++_primer_5e/ch16/ex16.48.49.50.cpp
@@ -38,3 +38,20 @@ int main(int argc, char **argv)
     debug_rep("hi");
     return 0;
 }
+
+
+// 16.49
+// template <typename T> void f(T);
+// template <typename T> void f(const T*);
+// template <typename T> void g(T);
+// template <typename T> void g(T*);
+// int i = 42, *p = &i;
+// const int ci = 0m *p2 = &ci
+// g(42) -> initialize g(T) with T as int
+// g(p)  -> initialize g(T*) with T as int
+// g(ci) -> initialize g(T) with T as const int
+// g(p2) -> initialize g(T*) with T as const int
+// f(42) -> initialize f(T) with T as int
+// f(p)  -> initialize f(T) with T as int*
+// f(ci) -> initialize f(T) with T as const int
+// f(p2) -> initialize f(const T*) with T as int
