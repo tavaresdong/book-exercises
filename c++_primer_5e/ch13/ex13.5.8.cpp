ffb178687ec51123ee1f2fd880e22e6e63969524
tavaresdong <tavares.nju@gmail.com>
2016-05-08 03:30:22 -0700

    ex13.8

--- c++_primer_5e/ch13/ex13.5.8.cpp
+++ c++_primer_5e/ch13/ex13.5.8.cpp
@@ -6,6 +6,11 @@ public:
     HasPtr(const HasPtr& other) : ps(new std::string(*other.ps)), i(other.i) {}
     void setPs(std::string* nps) { delete ps; ps = nps; }
     void print() { std::cout << *ps << " " << i << std::endl; }
+    HasPtr& operator=(HasPtr& rh) {
+        delete ps;
+        ps = new std::string(*rh.ps);
+        i = rh.i;
+    }
 private:
     std::string *ps;
     int i;
@@ -15,8 +20,10 @@ private:
 int main(int argc, char **argv) {
     HasPtr ptr1("good");
     HasPtr ptr2 = ptr1;
+    HasPtr ptr3("terrible");
     std::string* str2 = new std::string("happy");
     ptr2.setPs(str2);
+    ptr1 = ptr3;
     ptr1.print();
     ptr2.print();
 
