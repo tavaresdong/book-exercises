53419bb36bfc9c7f6170d27e43d6220289096191
tavaresdong <tavares.nju@gmail.com>
2016-05-30 05:42:25 -0700

    virtual is not needed in derived class

--- c++_primer_5e/ch15/Quote.h
+++ c++_primer_5e/ch15/Quote.h
@@ -26,7 +26,7 @@ class Bulk_quote : public Quote {
 public:
     Bulk_quote() = default;
     Bulk_quote(const std::string&, double, std::size_t, double);
-    virtual double net_price(std::size_t) const override;
+    double net_price(std::size_t) const override;
 protected:
     std::size_t min_qty = 0;
     double discount = 0.0;
