169aac2b7d49ecd5c5f42f05aa51db6280f49bd9
tavaresdong <tavares.nju@gmail.com>
2016-05-29 20:22:17 -0700

    limited bulk quote

--- c++_primer_5e/ch15/Quote.cpp
+++ c++_primer_5e/ch15/Quote.cpp
@@ -12,6 +12,22 @@ double Bulk_quote::net_price(std::size_t cnt) const
         return cnt * price;
 }
 
+Limited_bulk_quote::Limited_bulk_quote(const std::string& book, double p,
+                                       std::size_t qty, double disc,
+                                       std::size_t lmt) :
+                                       Bulk_quote(book, p, qty, disc),
+                                       limit(lmt) { }
+
+double Limited_bulk_quote::net_price(std::size_t cnt) const
+{
+    if (cnt >= min_qty && limit >= cnt)
+        return cnt * (1 - discount) * price;
+    else if (cnt > limit)
+        return limit * (1 - discount) * price + (cnt - limit) * price;
+    else
+        return cnt * price;
+}
+
 double print_total(std::ostream &os,
                    const Quote &item, size_t n)
 {
--- c++_primer_5e/ch15/Quote.h
+++ c++_primer_5e/ch15/Quote.h
@@ -26,10 +26,19 @@ class Bulk_quote : public Quote {
 public:
     Bulk_quote() = default;
     Bulk_quote(const std::string&, double, std::size_t, double);
-    double net_price(std::size_t) const override;
-private:
+    virtual double net_price(std::size_t) const override;
+protected:
     std::size_t min_qty = 0;
     double discount = 0.0;
+};
+
+class Limited_bulk_quote : public Bulk_quote {
+public:
+    Limited_bulk_quote() = default;
+    Limited_bulk_quote(const std::string&, double, std::size_t, double, std::size_t);
+    double net_price(std::size_t) const override;
+private:
+    std::size_t limit;
 };
 
 double print_total(std::ostream &os,
